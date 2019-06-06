633a0bfe7a2da0affca582ae1d4d4523b268b1bb
seeduardo <romenflatiron@gmail.com>
2019-06-06 15:20:11 +0100

    Lab completed

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function findMinAndRemove(array){
 
+  let min = array[0];
+  let minIndex = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
 }
 
 function insertionSort(array){
+
+  let sortedArray = [];
+  let current
+
+  while (array.length > 0) {
+    let current = findMinAndRemove(array)
+    sortedArray.push(current)
+  }
+  return sortedArray
 
 }
